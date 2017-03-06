f290eae5529139a7691dcbfa3413c0d598bed05a
karan mehta <karan.mehta@promactinfo.com>
2017-03-06 16:51:52 +0530

    added

---
+++ Angular2Demo/src/Angular2Demo/wwwroot/app/app.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.html',
+})
+export class AppComponent {
+   
+    constructor() {
+      //  return Http.get(./HomeController);
+    }
+}
